0d7a7001adb13ab506c4b73909c702f78694a627
MatthewR0616 <138822689+MatthewR0616@users.noreply.github.com>
2023-12-04 16:00:06 -0800

    reduce added

---
+++ findEx.js
@@ -0,0 +1,30 @@
+//findUserByUsername
+
+const users = [
+    {username: 'mlewis'},
+    {username: 'akagen'},
+    {username: 'msmith'}
+  ];
+
+  function findUserByUsername(usersArray, username) {
+    return usersArray.find(function(user){
+      return user.username === username;
+    })
+  }
+
+  //removeUser
+
+  const users = [
+    {username: 'mlewis'},
+    {username: 'akagen'},
+    {username: 'msmith'}
+  ];
+
+  function removeUser(usersArray, username) {
+    let foundIndex = usersArray.findIndex(function(user){
+      return user.username === username;
+    })
+    if(foundIndex === -1) return;
+  
+    return usersArray.splice(foundIndex,1)[0];
+  }
